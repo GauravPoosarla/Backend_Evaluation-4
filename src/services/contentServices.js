dee752cbec0ee0a6d89e2742b40e5c46e34c5d31
GauravPoosarla <poosarlagaurav@gmail.com>
2023-03-10 22:04:27 +0530

    feat: add tests of controllers and services

--- src/services/contentServices.js
+++ src/services/contentServices.js
@@ -1,185 +1,5 @@
 const db = require("../../database/models");
-
-const getAllCollections = async () => {
-  const collections = await db.Collection.findAll({
-    include: [
-      {
-        model: db.Content,
-      },
-    ],
-  });
-  console.log(collections);
-  return collections;
-};
-
-const getAllEntriesOfCollection = async (collectionId) => {
-  const entries = await db.Content.findAll({
-    where: {
-      collection_id: collectionId,
-    },
-    include: [
-      {
-        model: db.Collection,
-      },
-    ],
-  });
-  return entries;
-};
-
-const createCollection = async (collectionName) => {
-  // console.log(collectionName);
-  const collection = await db.Collection.create({
-    collection_name: collectionName,
-  });
-  return collection;
-};
-
-const updateCollection = async (collectionId, collectionName) => {
-  await db.Collection.update(
-    {
-      collection_name: collectionName,
-    },
-    {
-      where: {
-        id: collectionId,
-      },
-    }
-  );
-  const updatedCollection = await db.Collection.findOne({
-    where: {
-      id: collectionId,
-    },
-    include: [
-      {
-        model: db.Content,
-        attributes: ["values"],
-      },
-    ],
-  });
-  return updatedCollection;
-};
-
-const createContentFields = async (contentId, field) => {
-  const contentFields = await db.ContentField.create({
-    content_id: contentId,
-    fields: field,
-  });
-  return contentFields;
-};
-
-const getContentFields = async (contentId) => {
-  const contentFields = await db.ContentField.findAll({
-    where: {
-      content_id: contentId,
-    },
-  });
-  return contentFields;
-};
-
-const deleteContentField = async (fieldId) => {
-  const fieldName = await db.ContentField.findOne({
-    where: {
-      id: fieldId,
-    },
-  });
-  await db.ContentField.destroy({
-    where: {
-      id: fieldId,
-    },
-  });
-
-  const content = await db.Content.findAll();
-  content.forEach(async (item) => {
-    const values = item.values;
-    delete values[fieldName.fields];
-    await db.Content.update(
-      {
-        values: values,
-      },
-      {
-        where: {
-          id: item.id,
-        },
-      }
-    );
-  });
-};
-
-const updateContentField = async (contentId, fieldId, newFieldName) => {
-  const fieldName = await db.ContentField.findOne({
-    where: {
-      field_id: fieldId,
-    },
-  });
-
-  const field = fieldName.fields;
-  await db.ContentField.update(
-    {
-      fields: newFieldName,
-    },
-    {
-      where: {
-        content_id: contentId,
-        field_id: fieldId,
-      },
-    }
-  );
-  const content = await db.Content.findAll();
-  content.forEach(async (item) => {
-    const values = item.values;
-    values[newFieldName] = values[field];
-    delete values[field];
-    await db.Content.update(
-      {
-        values: values,
-      },
-      {
-        where: {
-          id: item.id,
-        },
-      }
-    );
-  });
-  return "updated field value";
-};
-
-const createContent = async (collectionId, content) => {
-  const newContent = await db.Content.create({
-    collection_id: collectionId,
-    values: content,
-  });
-  return newContent;
-};
-
-const getContent = async (contentId) => {
-  const content = await db.Content.findOne({
-    where: {
-      id: contentId,
-    },
-  });
-  return content;
-};
-
-const updateContent = async (contentId, content) => {
-  await db.Content.update(
-    {
-      values: content,
-    },
-    {
-      where: {
-        id: contentId,
-      },
-    }
-  );
-  const updatedContent = await db.Content.findOne({
-    where: {
-      id: contentId,
-    },
-  });
-  return updatedContent;
-};
-
-module.exports = {
+const {
   getAllCollections,
   getAllEntriesOfCollection,
   createCollection,
@@ -191,4 +11,111 @@ module.exports = {
   createContent,
   getContent,
   updateContent,
-};
+} = require("../../src/services/contentServices");
+
+jest.mock("../../database/models", () => ({
+  Collection: {
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    findOne: jest.fn(),
+  },
+  Content: {
+    findAll: jest.fn(),
+    create: jest.fn(),
+    update: jest.fn(),
+    findOne: jest.fn(),
+  },
+  ContentField: {
+    create: jest.fn(),
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    destroy: jest.fn(),
+    update: jest.fn(),
+  },
+}));
+
+describe("collectionService", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  describe("getAllCollections", () => {
+    it("should return all collections", async () => {
+      db.Collection.findAll.mockResolvedValue([{ id: 1, name: "collection1" }]);
+      const result = await getAllCollections();
+      expect(db.Collection.findAll).toHaveBeenCalled();
+      expect(result).toEqual([{ id: 1, name: "collection1" }]);
+    });
+
+    it("should return an empty array if there are no collections", async () => {
+      db.Collection.findAll.mockResolvedValue([]);
+      const result = await getAllCollections();
+      expect(db.Collection.findAll).toHaveBeenCalled();
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe("getAllEntriesOfCollection", () => {
+    it("should return all entries of a collection", async () => {
+      db.Content.findAll.mockResolvedValue([
+        { id: 1, collection_id: 1, values: {} },
+      ]);
+      const result = await getAllEntriesOfCollection(1);
+      expect(db.Content.findAll).toHaveBeenCalledWith({
+        where: { collection_id: 1 },
+        include: [{ model: db.Collection }],
+      });
+      expect(result).toEqual([{ id: 1, collection_id: 1, values: {} }]);
+    });
+
+    it("should return an empty array if there are no entries for the collection", async () => {
+      db.Content.findAll.mockResolvedValue([]);
+      const result = await getAllEntriesOfCollection(1);
+      expect(db.Content.findAll).toHaveBeenCalledWith({
+        where: { collection_id: 1 },
+        include: [{ model: db.Collection }],
+      });
+      expect(result).toEqual([]);
+    });
+  });
+
+  describe("createCollection", () => {
+    it("should create a new collection and return it", async () => {
+      db.Collection.create.mockResolvedValue({
+        id: 1,
+        collection_name: "collection1",
+      });
+      const result = await createCollection("collection1");
+      expect(db.Collection.create).toHaveBeenCalledWith({
+        collection_name: "collection1",
+      });
+      expect(result).toEqual({ id: 1, collection_name: "collection1" });
+    });
+  });
+
+  describe("updateCollection", () => {
+    it("should update an existing collection and return it", async () => {
+      db.Collection.update.mockResolvedValue(1);
+      db.Collection.findOne.mockResolvedValue({
+        id: 1,
+        collection_name: "collection1",
+        Content: [{ values: {} }],
+      });
+      const result = await updateCollection(1, "newName");
+      expect(db.Collection.update).toHaveBeenCalledWith(
+        { collection_name: "newName" },
+        { where: { id: 1 } }
+      );
+      expect(db.Collection.findOne).toHaveBeenCalledWith({
+        where: { id: 1 },
+        include: [{ model: db.Content, attributes: ["values"] }],
+      });
+      expect(result).toEqual({
+        id: 1,
+        collection_name: "newName",
+        Content: [{ values: {} }],
+      });
+    });
+  });
+});
---
+++ test/controllers/contentController.test.js
@@ -0,0 +1,196 @@
+const services = require("../../src/services/contentServices");
+const controller = require("../../src/controllers/contentController");
+
+describe("Content Controller", () => {
+  describe("getAllCollections", () => {
+    it("should return all collections", async () => {
+      const req = {};
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockCollections = [
+        {
+          collection_id: 1,
+          collection_name: "test",
+        },
+      ];
+      services.getAllCollections = jest.fn().mockResolvedValue(mockCollections);
+      await controller.getAllCollections(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(mockCollections);
+    });
+    it("should return error if getAllCollections fails", async () => {
+      const req = {};
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockError = new Error("error");
+      services.getAllCollections = jest.fn().mockRejectedValue(mockError);
+      await controller.getAllCollections(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ error: mockError.message });
+    });
+  });
+  describe("getAllEntriesOfCollection", () => {
+    it("should return all entries of collection", async () => {
+      const req = {
+        params: {
+          collectionId: 1,
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockEntries = [
+        {
+          content_id: 1,
+          collection_id: 1,
+          content_fields: {
+            field_id: 1,
+            content_id: 1,
+            fields: {
+              title: "test",
+            },
+          },
+        },
+      ];
+      services.getAllEntriesOfCollection = jest
+        .fn()
+        .mockResolvedValue(mockEntries);
+      await controller.getAllEntriesOfCollection(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(mockEntries);
+    });
+    it("should return error if getAllEntriesOfCollection fails", async () => {
+      const req = {
+        params: {
+          collectionId: 1,
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockError = new Error("error");
+      services.getAllEntriesOfCollection = jest
+        .fn()
+        .mockRejectedValue(mockError);
+      await controller.getAllEntriesOfCollection(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ error: mockError.message });
+    });
+  });
+  describe("createCollection", () => {
+    it("should create a collection", async () => {
+      const req = {
+        body: {
+          collectionName: "test",
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockCollection = {
+        collection_id: 1,
+        collection_name: "test",
+      };
+      services.createCollection = jest.fn().mockResolvedValue(mockCollection);
+      await controller.createCollection(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(mockCollection);
+    });
+    it("should return error if createCollection fails", async () => {
+      const req = {
+        body: {
+          collectionName: "test",
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockError = new Error("error");
+      services.createCollection = jest.fn().mockRejectedValue(mockError);
+      await controller.createCollection(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ error: mockError.message });
+    });
+    it("should return error if collectionName is not provided", async () => {
+      const req = {
+        body: {},
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      await controller.createCollection(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        error: "error",
+      });
+    });
+    it("should return error if collectionName is empty", async () => {
+      const req = {
+        body: {
+          collectionName: "",
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      await controller.createCollection(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({
+        error: "error",
+      });
+    });
+  });
+  describe("updateCollection", () => {
+    it("should update a collection", async () => {
+      const req = {
+        params: {
+          collectionId: 1,
+        },
+        body: {
+          collection_name: "test",
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockCollection = {
+        collection_id: 1,
+        collection_name: "test",
+      };
+      services.updateCollection = jest.fn().mockResolvedValue(mockCollection);
+      await controller.updateCollection(req, res);
+      expect(res.status).toHaveBeenCalledWith(200);
+      expect(res.json).toHaveBeenCalledWith(mockCollection);
+    });
+    it("should return error if updateCollection fails", async () => {
+      const req = {
+        params: {
+          collectionId: 1,
+        },
+        body: {
+          collection_name: "test",
+        },
+      };
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const mockError = new Error("error");
+      services.updateCollection = jest.fn().mockRejectedValue(mockError);
+      await controller.updateCollection(req, res);
+      expect(res.status).toHaveBeenCalledWith(500);
+      expect(res.json).toHaveBeenCalledWith({ error: mockError.message });
+    });
+  });
+});
